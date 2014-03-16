5b3c5fa7f288f36b0a8d443e312b84e532d1b156
Andrew Hilobok <hilobok@gmail.com>
2014-03-16 17:12:48 +0200

    Mark version 1.0.3

--- AnhTiedContentBundle.php
+++ AnhTiedContentBundle.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AnhTiedContentBundle extends Bundle
 {
-    const VERSION = 'v1.0.1';
+    const VERSION = 'v1.0.3';
     const TITLE = 'AnhTiedContentBundle';
     const DESCRIPTION = 'Bundle for tied content management';
 
